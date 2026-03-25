7949dc87a470f1afd9b59957434c7bcfcec1a1f5
Jose Valduz <jose.valduz@unet.edu.ve>
2026-03-25 15:40:23 -0400

    Implement actualizar method in ClienteDAO for updating client records in the database

--- zona_fit_db/cliente_dao.py
+++ zona_fit_db/cliente_dao.py
@@ -47,14 +47,35 @@ class ClienteDAO:
                 cursor.close()
                 Conexion.liberar_conexion(conexion)
 
+    @classmethod
+    def actualizar(cls, cliente):
+        conexion = None
+        try:
+            conexion = Conexion.obtener_conexion()
+            cursor = conexion.cursor()
+            valores = (cliente.nombre, cliente.apellido, cliente.membresia, cliente.id)
+            cursor.execute(cls.ACTUALIZAR, valores)
+            conexion.commit()
+            return cursor.rowcount#Valores modificados en la base de datos
+        except Exception as e:
+            print(f"Error al actualizar cliente: {e}")
+        finally:
+            if conexion is not None:
+                cursor.close()
+                Conexion.liberar_conexion(conexion)
+
 # Pruebas
 if __name__ == "__main__":
-    # Insertar clientes
-    print("-"*50)
-    cliente1 = Cliente(nombre="Juan", apellido="Perez", membresia=300)
-    clientes_actualizados = ClienteDAO.insertar(cliente1)
-    print(f"Clientes insertados: {clientes_actualizados}")
-    print("-"*50)
+    # # Insertar clientes
+    # print("-"*50)
+    # cliente1 = Cliente(nombre="Juan", apellido="Perez", membresia=300)
+    # clientes_actualizados = ClienteDAO.insertar(cliente1)
+    # print(f"Clientes insertados: {clientes_actualizados}")
+    # print("-"*50)
+    #Actualizar CLiente
+    cliente_actualizar = Cliente(2,"Alexa", "Perez", 600)
+    clientes_actualizados = ClienteDAO.actualizar(cliente_actualizar)
+    print(f"Clientes actualizados: {clientes_actualizados}")
     # seleccionar clientes
     clientes = ClienteDAO.seleccionar()
     for cliente in clientes:
